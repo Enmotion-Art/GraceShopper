941c3dc118dbbfc82ba363d92e7a4ff3d962e2a3
Adrienne Johnson <adriennetjohnson@gmail.com>
2018-10-30 21:18:35 -0400

    Added details on SingleArt and Shop link to Navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -14,6 +14,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {isLoggedIn ? (
             <div>
               {/* The navbar will show these links after you log in */}
+              <Link to="/">Shop</Link>
               <Link to="/home">Home</Link>
               <a href="#" onClick={handleClick}>
                 Logout
@@ -22,6 +23,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           ) : (
             <div>
               {/* The navbar will show these links before you log in */}
+              <Link to="/">Shop</Link>
               <Link to="/login">Login</Link>
               <Link to="/signup">Sign Up</Link>
             </div>
