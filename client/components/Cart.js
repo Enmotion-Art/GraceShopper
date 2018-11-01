1ec7c2a37bc8162b8ebc427dd5fcb669b5e8cf45
Adrienne Johnson <adriennetjohnson@gmail.com>
2018-10-31 22:29:03 -0400

    Functional Checkout Form - Successful Order Placement

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -2,40 +2,46 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import { withRouter, Link } from 'react-router-dom'
 import { fetchSingleArt, removeArt } from '../store/art'
-import Axios from 'axios';
-
+import { postOrder } from '../store/order'
 
 class Cart extends Component {
+  constructor() {
+    super()
+    this.handleCheckout = this.handleCheckout.bind(this)
+  }
 
-async handleCheckout(event) {
-    event.preventDefault();
-    await Axios.post('/api/orders', JSON.parse(localStorage.getItem('product')))
-    }
-    //This should happen through our reducer!
+  handleCheckout() {
+    this.props.createOrder(JSON.parse(localStorage.getItem('product')))
+  }
 
   render() {
     const cart = JSON.parse(localStorage.getItem('product'))
-    // const user = this.props.user
     console.log('this.props AGAIN', this.props)
-    return (
-      <div>
-        <br />
-        <h1>{cart.title}</h1>
-        <div id="container-row">
-          <div id="column">
-            <img src = {cart.image} />
-          </div>
-          <div id="second-column">
-            <p>{cart.description}</p>
-            <p>Style: {cart.category}</p>
-            <p>{cart.width}W x {cart.height}H</p>
-            <p><strong>${cart.price}</strong></p>
-            { cart.quantity === 0 ? <p>Nothing in your cart!</p> :
-            <button type="submit" onClick={this.handleCheckout}>Checkout</button> }
+    if(!cart) {
+      return (
+        <p>Your cart is empty.</p>
+      )
+    } else {
+      return (
+        <div>
+          <br />
+          <h1>{cart.title}</h1>
+          <div id="container-row">
+            <div id="column">
+              <img src = {cart.image} />
+            </div>
+            <div id="second-column">
+              <p>{cart.description}</p>
+              <p>Style: {cart.category}</p>
+              <p>{cart.width}W x {cart.height}H</p>
+              <p><strong>${cart.price}</strong></p>
+              { cart.quantity === 0 ? <p>Nothing in your cart!</p> :
+              <button type="submit" onClick={this.handleCheckout}>Checkout</button> }
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 }
 
@@ -44,17 +50,8 @@ const mapStateToProps = state => {
     user: state.user
   }
 }
-const mapDispatchToProps = dispatch => {
-  return {
-    actions: {
-      loadSingleArt: function(id) {
-        dispatch(fetchSingleArt(id))
-    },
-    removeSpecificArt: function(art) {
-      dispatch(removeArt(art))
-    }
-    }
-  }
-}
 
+const mapDispatchToProps = dispatch => ({
+  createOrder: (art) => dispatch(postOrder(art))
+})
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Cart))
--- server/api/orders.js
+++ server/api/orders.js
@@ -6,9 +6,6 @@ router.post('/', async (req, res, next) => {
   try {
     const order = await Order.create({
         status: 'created',
-        subtotal: 3,
-        street: 'Macdonough St',
-        contactEmail: 'mags21walker@gmail.com'
     })
     const id = req.body.id
     const currArt = await Art.findById(id)
@@ -27,7 +24,7 @@ router.get('/', async (req, res, next) => {
       next(err)
     }
   })
-  
+
 //   router.get('/:orderId', async (req, res, next) => {
 //     try {
 //       const order = await Order.findById(req.params.orderId);
@@ -37,7 +34,7 @@ router.get('/', async (req, res, next) => {
 //     }
 //   })
 
-  
+
   //When requesting this route on the checkout button, pass in 'created'; when requesting this route on submit order, pass in 'processing' as the second parameter.
   router.put('/:orderId', async (req, res, next) => {
     try {
