d54641ef86b6cf3b7c7fda6327e3ac70c07e8e76
Erica Luong <huongie2lu@gmail.com>
2018-11-01 14:38:32 -0400

    in progress Category Filter

--- client/components/AllArt.js
+++ client/components/AllArt.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { NavLink, withRouter } from 'react-router-dom'
 import { fetchAllArt } from '../store/art'
 import Pagination from './Pagination';
@@ -21,31 +21,41 @@ class AllArt extends Component {
   }
 
 
-  artFilter (category) {
+  artFilter(category) {
     const allArt = this.props.allArt
 
     if (category === 'priceAll') return allArt
-    if (category === 'priceOne') return allArt.filter(art => art.price<100)
-    if (category === 'priceTwo') return allArt.filter(art => art.price<301 && art.price>=100)
-    if (category === 'priceThree') return allArt.filter(art => art.price<500 && art.price>300)
-    if (category === 'priceFour') return allArt.filter(art => art.price>500)
+    if (category === 'priceOne') return allArt.filter(art => art.price < 100)
+    if (category === 'priceTwo') return allArt.filter(art => art.price < 301 && art.price >= 100)
+    if (category === 'priceThree') return allArt.filter(art => art.price < 500 && art.price > 300)
+    if (category === 'priceFour') return allArt.filter(art => art.price > 500)
+  }
+
+  categoryFilter(category) {
+    const allArt = this.props.allArt
+
+    if (category === 'priceAll') return allArt
+    if (category === 'priceOne') return allArt.filter(art => art.price < 100)
+    if (category === 'priceTwo') return allArt.filter(art => art.price < 301 && art.price >= 100)
+    if (category === 'priceThree') return allArt.filter(art => art.price < 500 && art.price > 300)
+    if (category === 'priceFour') return allArt.filter(art => art.price > 500)
   }
 
   handleSelect(event) {
     const category = event.target.value
     this.setState({
-        [event.target.name]: this.artFilter(category)
-       });
-   }
+      [event.target.name]: this.artFilter(category)
+    });
+  }
 
-   handlePageChange(page) {
-     const renderedArt = this.props.allArt.slice((page-1)*2, (page-1) *2 + 2)
-     this.setState({
-       renderedArt: renderedArt,
-       activePage: page
-      });
+  handlePageChange(page) {
+    const renderedArt = this.props.allArt.slice((page - 1) * 2, (page - 1) * 2 + 2)
+    this.setState({
+      renderedArt: renderedArt,
+      activePage: page
+    });
 
-   }
+  }
 
   render() {
     const allArt = this.props.allArt
@@ -55,38 +65,48 @@ class AllArt extends Component {
       <div>
 
         <div>
-            <label>Filter by Price</label>
-            <select name='selectedArt' onChange={this.handleSelect}>
-                <option value='priceAll'>All Prices</option>
-                <option value='priceOne'  >Under $100</option>
-                <option value='priceTwo'  >$100 - $300 </option>
-                <option value='priceThree'  > $301 - $500</option>
-                <option value='priceFour'  > Over $500</option>
-            </select>
+          <label>Filter by Price</label>
+          <select name='selectedArt' onChange={this.handleSelect}>
+            <option value='priceAll'>All Prices</option>
+            <option value='priceOne'  >Under $100</option>
+            <option value='priceTwo'  >$100 - $300 </option>
+            <option value='priceThree'  > $301 - $500</option>
+            <option value='priceFour'  > Over $500</option>
+          </select>
+        </div>
+
+        <div>
+          <label>Filter by Category</label>
+          <select name='selectedArt' onChange={this.handleSelect}>
+            <option value='realism'>Realism</option>
+            <option value='psyche'  >Psyche</option>
+            <option value='socialJustice'  >Social Justice</option>
+            <option value='inspiredBy'  > Inspired by... </option>
+          </select>
         </div>
 
         <div>
           <h1>Buy Art! Feel Special</h1>
-            {
-              selectedArt ?
+          {
+            selectedArt ?
 
-                selectedArt.map(art =>
-                <div key ={art.id}>
-                <NavLink to={`/art/${art.id}`}> {art.title} </NavLink>
-                <img src = {art.image} />
+              selectedArt.map(art =>
+                <div key={art.id}>
+                  <NavLink to={`/art/${art.id}`}> {art.title} </NavLink>
+                  <img src={art.image} />
                 </div>
-                )
-             :
-                allArt.map(art =>
-                <div key ={art.id}>
-                <NavLink to={`/art/${art.id}`}> {art.title} </NavLink>
-                <img src = {art.image} />
+              )
+              :
+              allArt.map(art =>
+                <div key={art.id}>
+                  <NavLink to={`/art/${art.id}`}> {art.title} </NavLink>
+                  <img src={art.image} />
                 </div>
-                )
-            }
+              )
+          }
         </div>
 
-        <Pagination allArt={allArt}/>
+        <Pagination allArt={allArt} />
 
       </div>
     )
@@ -101,7 +121,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    loadInitialArt: function() {
+    loadInitialArt: function () {
       dispatch(fetchAllArt())
     }
   }
