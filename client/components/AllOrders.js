73bfcbad67cc92551c6f6db5bc9faa7758835046
LE (Lee) <lm61613@gmail.com>
2018-11-02 17:08:55 -0400

    Yay tables

--- client/components/AllOrders.js
+++ client/components/AllOrders.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import { NavLink, withRouter } from 'react-router-dom'
-import { fetchAllOrders } from '../store/order'
-
+import {NavLink, withRouter} from 'react-router-dom'
+import {fetchAllOrders} from '../store/order'
 
 class AllOrders extends Component {
-
   constructor(props) {
     super(props)
     this.state = {
-      selectedOrders: null, //inital null value to account for when an order doesn't match any category. Has to do with if/else stmt in the render. Subject to change.
+      selectedOrders: null //inital null value to account for when an order doesn't match any category. Has to do with if/else stmt in the render. Subject to change.
     }
     this.handleSelect = this.handleSelect.bind(this)
   }
@@ -18,60 +16,125 @@ class AllOrders extends Component {
     this.props.loadInitialOrders()
   }
 
-    orderFilter (status) {
+  orderFilter(status) {
     const allOrders = this.props.allOrders
 
-    if (status === 'created') return allOrders.filter(order => order.status === 'created')
-    if (status === 'processing') return allOrders.filter(order => order.status === 'processing')
-    if (status === 'shipped') return allOrders.filter(order => order.status === 'shipped')
-    if (status === 'cancelled') return allOrders.filter(order => order.status === 'cancelled')
+    if (status === 'created')
+      return allOrders.filter(order => order.status === 'created')
+    if (status === 'processing')
+      return allOrders.filter(order => order.status === 'processing')
+    if (status === 'shipped')
+      return allOrders.filter(order => order.status === 'shipped')
+    if (status === 'cancelled')
+      return allOrders.filter(order => order.status === 'cancelled')
   }
 
   handleSelect(event) {
     const status = event.target.value
     this.setState({
-        [event.target.name]: this.orderFilter(status)
-       });
-   }
+      [event.target.name]: this.orderFilter(status)
+    })
+  }
 
   render() {
     const orders = this.props.allOrders
     const selectedOrders = this.state.selectedOrders
 
-      return (
-        <div className='grid'>
-
-          <div className='grid-child'>
-            <label>Filter by Status</label>
-            <select name='selectedOrders' onChange={this.handleSelect}>
-                <option></option>
-                <option value='created'>Created</option>
-                <option value='processing'  >Processing</option>
-                <option value='shipped'  >Shipped</option>
-                <option value='cancelled'  > Cancelled</option>
-            </select>
+    return (
+      <div className="grid">
+        <div className="grid-child">
+          <label>Filter by Status</label>
+          <select name="selectedOrders" onChange={this.handleSelect}>
+            <option />
+            <option value="created">Created</option>
+            <option value="processing">Processing</option>
+            <option value="shipped">Shipped</option>
+            <option value="cancelled"> Cancelled</option>
+          </select>
         </div>
-
-          {
-            selectedOrders ?
-
-            selectedOrders.map(order =>
-             <div className='grid-child' key ={order.id}>
-              <NavLink to={`/order/${order.id}`}> {order.id} </NavLink>
-              <div>ALSO RENDER DETAILS OF ORDER HERE</div>
-             </div>
-             )
-
-            :
-            orders.map(order =>
-            <div className='grid-child' key={order.id}>
-            <NavLink to={`/orders/${order.id}`}> {order.id} </NavLink>
-            <div>ALSO RENDER DETAILS OF ORDER HERE</div>
-            </div>
-              )
-          }
-        </div>
-      )
+        <table class="blueTable">
+          <thead>
+            <tr>
+              <th>Order ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Status</th>
+              <th>Subtotal</th>
+              <th>Street Num</th>
+              <th>Street</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Zip</th>
+              <th>Created At</th>
+              <th>Updated At</th>
+              <th>User ID</th>
+            </tr>
+          </thead>
+          {/* <tfoot>
+            <tr>
+              <td colspan="14">
+                <div class="links">
+                  <a href="#">&laquo;</a>{' '}
+                  <a class="active" href="#">
+                    1
+                  </a>{' '}
+                  <a href="#">2</a> <a href="#">3</a> <a href="#">4</a>{' '}
+                  <a href="#">&raquo;</a>
+                </div>
+              </td>
+            </tr>
+          </tfoot> */}
+          <tbody>
+            {selectedOrders
+              ? selectedOrders.map(order => (
+                  // <div className='grid-child' key ={order.id}>
+                  //   <NavLink to={`/order/${order.id}`}> {order.id} </NavLink>
+                  //   <div>ALSO RENDER DETAILS OF ORDER HERE</div>
+                  // </div>
+                  <tr key={order.id}>
+                    <td as={NavLink} to={`/order/${order.id}`}>{order.id}</td>
+                    <td>{order.firstName}</td>
+                    <td>{order.lastName}</td>
+                    <td>{order.email}</td>
+                    <td>{order.status}</td>
+                    <td>{order.subtotal}</td>
+                    <td>{order.streetNum}</td>
+                    <td>{order.street}</td>
+                    <td>{order.city}</td>
+                    <td>{order.state}</td>
+                    <td>{order.zip}</td>
+                    <td>{order.createdAt}</td>
+                    <td>{order.updatedAt}</td>
+                    <td>{order.userId}</td>
+                  </tr>
+                ))
+              : orders.map(order => (
+                  // <div className='grid-child' key={order.id}>
+                  // <NavLink to={`/orders/${order.id}`}> {order.id} </NavLink>
+                  // <div>ALSO RENDER DETAILS OF ORDER HERE</div>
+                  // </div>
+                  <tr key={order.id}>
+                    <td as={NavLink} to={`/order/${order.id}`}>{order.id}</td>
+                    <td>{order.firstName}</td>
+                    <td>{order.lastName}</td>
+                    <td>{order.email}</td>
+                    <td>{order.status}</td>
+                    <td>{order.subtotal}</td>
+                    <td>{order.streetNum}</td>
+                    <td>{order.street}</td>
+                    <td>{order.city}</td>
+                    <td>{order.state}</td>
+                    <td>{order.zip}</td>
+                    <td>{order.createdAt}</td>
+                    <td>{order.updatedAt}</td>
+                    <td>{order.userId}</td>
+                  </tr>
+                ))}
+          </tbody>
+        </table>
+      </div>
+    )
   }
 }
 
--- client/components/AllUsers.js
+++ client/components/AllUsers.js
@@ -1,42 +1,75 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import { NavLink, withRouter } from 'react-router-dom'
-import { fetchAllUsers, deleteThisUser } from '../store/user'
-
+import {NavLink, withRouter} from 'react-router-dom'
+import {fetchAllUsers, deleteThisUser} from '../store/user'
 
 class AllUsers extends Component {
   constructor() {
-    super();
+    super()
     this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
     this.props.actions.loadInitialUsers()
   }
-handleClick(event) {
-  event.preventDefault();
-  const UserId = event.target.id;
-  this.props.actions.removeSpecificUser({ id: UserId });
+  handleClick(event) {
+    event.preventDefault()
+    const UserId = event.target.id
+    this.props.actions.removeSpecificUser({id: UserId})
   }
 
   render() {
     const users = this.props.allUsers
 
-      return (
-        <div className='grid'>
-          {
-            users.map(user =>
-            <div className='grid-child' key={user.id}>
-            <NavLink to={`/user/${user.id}`}> {user.id} </NavLink>
-            <button type="button" id={`${user.id}`} onClick={this.handleClick}> 
-            X 
-            </button>
-            <div>ALSO RENDER DETAILS OF USER HERE (INCLUDING THAT USERS ORDERS?)</div>
-            </div>
-              )
-          }
-        </div>
-      )
+    return (
+      <div className="grid">
+        <table class="blueTable">
+          <thead>
+            <tr>
+              <th>User ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>User Type</th>
+              <th>Street Num</th>
+              <th>Street</th>
+              <th>City</th>
+              <th>State</th>
+              <th>Zip</th>
+              <th>Created At</th>
+              <th>Updated At</th>
+              <th>Remove User</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => (
+              // <div className='grid-child' key={user.id}>
+              // <NavLink to={`/user/${user.id}`}> {user.id} </NavLink>
+              // <button type="button" id={`${user.id}`} onClick={this.handleClick}>
+              // X
+              // </button>
+              // <div>ALSO RENDER DETAILS OF USER HERE (INCLUDING THAT USERS ORDERS?)</div>
+              // </div>
+              <tr key={user.id}>
+                <td as={NavLink} to={`/user/${user.id}`}>{user.id}</td>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.email}</td>
+                <td>{user.userType}</td>
+                <td>{user.streetNum}</td>
+                <td>{user.street}</td>
+                <td>{user.city}</td>
+                <td>{user.state}</td>
+                <td>{user.zip}</td>
+                <td>{user.createdAt}</td>
+                <td>{user.updatedAt}</td>
+                <td><button type="button" id={`${user.id}`} onClick={this.handleClick}> X </button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
   }
 }
 
@@ -51,10 +84,10 @@ const mapDispatchToProps = dispatch => {
     actions: {
       loadInitialUsers: function() {
         dispatch(fetchAllUsers())
-    },
-    removeSpecificUser: function(user) {
-      dispatch(deleteThisUser(user))
-    }
+      },
+      removeSpecificUser: function(user) {
+        dispatch(deleteThisUser(user))
+      }
     }
   }
 }
