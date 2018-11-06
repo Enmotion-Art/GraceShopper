144ee1f113954827dcbc42523a39e576c01029e5
Erica Luong <huongie2lu@gmail.com>
2018-11-06 11:22:23 -0500

    Feature: Toggle Add Art Form

--- client/components/AdminPage.js
+++ client/components/AdminPage.js
@@ -4,25 +4,47 @@ import AllOrders from './AllOrders'
 import AllUsers from './AllUsers'
 
 class AdminPage extends Component {
-    render() {
-        return (
-          <div className='admin-grid'>
-            <div className='admin-grid-child'>
-              <AddArt title='Add A New Piece Of Art'/>
-            </div>
-             <div className='admin-grid-child'>
-              <div>
-                <h2 className='yellow'>Orders:</h2>
-                <AllOrders />
-              </div>
-              <div>
-                <h2 className='yellow'>Users:</h2>
-                <AllUsers />
-            </div>
-            </div>
-          </div>
-        )
+  constructor() {
+    super()
+    this.state = {
+      showAddArtForm: false
     }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState({
+      showAddArtForm: !this.state.showAddArtForm
+    });
+  }
+
+  render() {
+    return (
+      <div className='admin-grid'>
+        <div className='admin-grid-child'>
+          <div><button type='button' onClick={this.handleClick}>Add Art</button></div>
+          {
+            this.state.showAddArtForm ?
+              <AddArt title='Add A New Piece Of Art' />
+              :
+              null
+          }
+
+        </div>
+        <div className='admin-grid-child'>
+          <div>
+            <h2 className='yellow'>Orders:</h2>
+            <AllOrders />
+          </div>
+          <div>
+            <h2 className='yellow'>Users:</h2>
+            <AllUsers />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default AdminPage
