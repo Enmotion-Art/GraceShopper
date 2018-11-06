e23761f70140f733961146b55a011261c5440e54
Maggie Walker <mags21walker@gmail.com>
2018-11-06 09:33:27 -0500

    Feature: Stripe Checkout - Renders Card Inputs

--- client/app.js
+++ client/app.js
@@ -4,13 +4,12 @@ import { Switch, Route } from 'react-router-dom'
 import { Redirect } from 'react-router'
 import LandingPage from './components/LandingPage'
 
-
 const App = () => {
   return (
       <Switch>
-          <Redirect exact path='/' to='/landingPage' />
-          <Route exact path ='/landingPage' component={LandingPage} />
-          <Route component={Routes} />
+            <Redirect exact path='/' to='/landingPage' />
+            <Route exact path ='/landingPage' component={LandingPage} />
+            <Route component={Routes} />
       </Switch>
   )
 }
--- client/components/CheckoutForm.js
+++ client/components/CheckoutForm.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { putOrder, postOrder } from '../store/order'
+import {StripeProvider} from 'react-stripe-elements'
+
+import MyStoreCheckout from './MyStoreCheckout'
 
 class CheckoutForm extends React.Component {
   constructor(props) {
@@ -97,6 +100,11 @@ class CheckoutForm extends React.Component {
 
   render() {
     return (
+      <div className="main-container">
+      <StripeProvider apiKey="key">
+      <MyStoreCheckout />
+      </StripeProvider>
+    
       <div className='grid' id="container-row">
         <div className='grid-child'>
           <form>
@@ -157,6 +165,7 @@ class CheckoutForm extends React.Component {
             </div>
           </div>
         </div>
+      </div>
       </div>
       )
     }
